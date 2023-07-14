2484a4ed2c1546bfce5e56ddac508dc014aab84d
Vibhanshu Garg <64217477+vibhanshu2001@users.noreply.github.com>
2023-07-14 15:55:01 +0530

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -1,7 +1,7 @@
 // newrelic.js
 exports.config = {
     app_name: ['dummy_proj'],
-    license_key: '92fcf3e0496f9a78e753bc62ba24bfe24ca8NRAL',
+    license_key: '',
     logging: {
         level: 'info'
     }
